964db8833a17b30a619952df1d93706fce153135
v1shwa <v1shwa@users.noreply.github.com>
2017-07-12 13:18:00 +0530

    Allow jupyter to run as root

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,4 +4,5 @@ from IPython.lib import passwd
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.allow_root = True
 c.NotebookApp.password = passwd(os.environ['NB_PASSWORD'])
